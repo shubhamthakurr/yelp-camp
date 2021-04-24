a6f08c2b955258c5b6199caa0cad3d4b4eb0907e
Shubham Thakur <38060542+shubhamthakurr@users.noreply.github.com>
2021-04-25 03:48:58 +0530

    Done style clean up

--- app.js
+++ app.js
@@ -54,6 +54,8 @@ const sessionConfig = {
 app.use(session(sessionConfig));
 app.use(flash());
 
+
+
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
